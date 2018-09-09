75e31f06a6fc3f7ae872e637c6e6c129cc96b7fd
Adrien Baudet <baudetad@gmail.com>
2018-09-09 16:42:46 +0200

    Started Lexer

---
+++ source/Lexer.hx
@@ -0,0 +1,95 @@
+package;
+using StringTools;
+
+/**
+ * ...
+ * @author 
+ */
+enum Token 
+{
+	Fun_def(name:String, args:Array<String>);
+	Fun_call(name:String, args:Array<Token>);
+	Var_def(name:String, value:Token);
+	Number(value:Int);
+	Returning(value:Token);
+	End_block;
+	Undef;
+}
+ 
+class Lexer 
+{
+	private var code:String;
+	
+	static private function to_token(chaine:String):Token
+	{
+		chaine = chaine.trim();
+		
+		// Function definition
+		var regex = ~/FUNCTION\s([A-Z_]+)\(([A-Z_, ]*)\)\sDO/;
+		if (regex.match(chaine))
+		{
+			return Fun_def(regex.matched(1), regex.matched(2).split(",").map(StringTools.trim));
+		}
+		
+		//Variable definition
+		var regex = ~/([A-Z_]+)\s=\s(.+)/;
+		if (regex.match(chaine))
+		{
+			return Var_def(regex.matched(1), to_token(regex.matched(2)));
+		}
+		
+		//Return a value
+		var regex = ~/RETURN(.+)/;
+		if (regex.match(chaine))
+		{
+			return Returning(to_token(regex.matched(1)));
+		}
+		
+		//Define a number
+		var regex = ~/([0-9]+)/;
+		if (regex.match(chaine))
+		{
+			return Number(Std.parseInt(regex.matched(1)));
+		}
+		
+		//End keyword
+		var regex = ~/END/;
+		if (regex.match(chaine))
+		{
+			return End_block;
+		}
+		
+		//Function call
+		var regex = ~/([A-Z_]+)\((.*)\)/i;
+		if (regex.match(chaine))
+		{
+			var temp = regex.matched(2).trim();
+			if (temp == "")
+			{
+				return Fun_call(regex.matched(1), []);
+			}
+			else
+			{
+				return Fun_call(regex.matched(1), regex.matched(2).split(",").map(to_token));
+			}
+			
+		}
+		return Undef;
+	}
+
+	public function new(code) 
+	{
+		this.code = code;
+	}
+	
+	public function lex()
+	{
+		var result = code.split("\n").map(StringTools.trim).map(to_token);
+		for (i in result)
+		{
+			trace(i);
+		}
+		
+		return result;
+	}
+}
