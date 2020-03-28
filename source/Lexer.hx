caf205b8927e6c9e2904eb804825529f33906f58
Adrien Baudet <baudetad@gmail.com>
2020-03-28 09:20:53 +0100

    changed lexer

--- source/Lexer.hx
+++ source/Lexer.hx
@@ -8,118 +8,143 @@ using StringTools;
 enum Token 
 {
 	Integ(val:Int);
-	Decimal(val:Float);
+	//Decimal(val:Float);
 	Boolean(val:Bool);
 	Str(val:String);
 	Identifier(val:String);
-	OpenParen;
-	ClosePar;
-	OpenBracket;
-	CloseBracket;
-	
+	Group(val: Array<Token>);
 }
  
 class Lexer 
 {
-	private var code:String;
-	
-	static private function to_token(chaine:String, acc:Array<Token>):Array<Token>
-	{		
+	static public function to_token(chaine:String): Token
+	{
+		
 		if (chaine == "") 
 		{
-			return acc;
+			return Integ(42);
 		}
 		
-		// Blank
-		var regex = ~/^\s+/;
+		if (chaine.charAt(0) == '(') 
+		{
+			return Group(lex(chaine.substring(1, chaine.length - 1)));
+		}
+		
+		if (chaine.charAt(0) == '"') 
+		{
+			return Str(chaine.substring(1, chaine.length - 1));
+		}
+		
+		if ((chaine == 'true') || (chaine == 'false')) 
+		{
+			return Boolean(chaine == 'true');
+		}
+		
+		var regex = ~/^[A-Za-z][A-Za-z0-9]*$/;
 		if (regex.match(chaine))
 		{
-			return to_token(regex.matchedRight(), acc);
+			return Identifier(chaine);
 		}
 		
-		// Integer
-		var regex = ~/^([0-9]+)/;
+		var regex = ~/^[+\-*\/!<>=|&]*$/;
 		if (regex.match(chaine))
 		{
-			var token = Integ(Std.parseInt(regex.matched(1)));
-			acc.push(token);
-			return to_token(regex.matchedRight(), acc);
+			return Identifier(chaine);
 		}
 		
-		// Float
-		var regex = ~/^([0-9]*.[0-9]+)/;
-		if (regex.match(chaine))
-		{
-			var token = Decimal(Std.parseFloat(regex.matched(1)));
-			acc.push(token);
-			return to_token(regex.matchedRight(), acc);
-		}
-		
-		
-		// Identifier
-		var regex = ~/^([A-Za-z][A-Za-z0-9!]*)/;
-		if (regex.match(chaine))
-		{
-			var token = Identifier(regex.matched(1));
-			acc.push(token);
-			return to_token(regex.matchedRight(), acc);
-		}
-		
-		// Open parenthesis
-		var regex = ~/^\(/;
-		if (regex.match(chaine))
-		{
-			var token = OpenParen;
-			acc.push(token);
-			return to_token(regex.matchedRight(), acc);
-		}
-		
-		// Close parenthesis
-		var regex = ~/^\)/;
-		if (regex.match(chaine))
-		{
-			var token = ClosePar;
-			acc.push(token);
-			return to_token(regex.matchedRight(), acc);
-		}
-		
-		// Open bracket
-		var regex = ~/^\[/;
-		if (regex.match(chaine))
-		{
-			var token = OpenBracket;
-			acc.push(token);
-			return to_token(regex.matchedLeft(), acc);
-		}
-		
-		// Open parenthesis
-		var regex = ~/^\]/;
-		if (regex.match(chaine))
-		{
-			var token = CloseBracket;
-			acc.push(token);
-			return to_token(regex.matchedRight(), acc);
-		}
-		
-		throw "uknown text";
-	}
-
-	public function new(code) 
-	{
-		this.code = code;
+		throw 'Uknown token ${chaine}';
 	}
 	
-	public function lex()
+	static private function manage_paren(code:String, pointer:Int): Int
 	{
-		//trace(code);
-		var result = to_token(code, []);
-		//var result = code.split("\n").map(StringTools.trim).map(to_token).filter(function(x){return x != Undef; });
-		for (i in result)
+		var initial = pointer;
+		pointer++;
+		var count = 1;
+		while ((count > 0) && (pointer < code.length)) 
 		{
-			trace(i);
+			if (code.charAt(pointer) == '(')
+			{
+				count++;
+			}
+			else if (code.charAt(pointer) == ')')
+			{
+				count--;
+			}
+			pointer++;
 		}
 		
-		trace(result);
-		return result;
+		if ((pointer == code.length) && (count > 0)) 
+		{
+			throw "Missing )";
+		}
+		
+		return pointer - initial;
+	}
+	
+	static private function manage_space(code:String, pointer:Int): Int
+	{
+		var initial = pointer;
+		pointer ++;
+		while ((code.charAt(pointer) != ' ') && (pointer < code.length)) 
+		{
+			pointer++;
+		}
+		
+		return pointer - initial;
+	}
+	
+	static private function manage_string(code:String, pointer:Int):Int
+	{
+		var regex = ~/^"[^"]*"/;
+		//var regex = ~/^"(([^"]|\\")*)"/;
+		if (regex.matchSub(code, pointer))
+		{
+			return regex.matched(0).length;
+		}
+		
+		throw 'Missing "';
+	}
+	
+	static public function lex(code: String): Array<Token>
+	{		
+		var acc:Array<String> = [];
+		var pointer = 0;
+		trace(code);
+		
+		while (pointer < code.length) 
+		{
+			var regex = ~/^\s+/;
+			if (regex.matchSub(code, pointer))
+			{
+				pointer++;
+				continue;
+			}
+			
+			if (code.charAt(pointer) == '(') 
+			{
+				var size = manage_paren(code, pointer);
+				acc.push(code.substr(pointer, size));
+				pointer += size;
+			}
+			else if (code.charAt(pointer) == '"') 
+			{
+				var size = manage_string(code, pointer);
+				acc.push(code.substr(pointer, size));
+				pointer += size;
+			}
+			else
+			{
+				var size = manage_space(code, pointer);
+				acc.push(code.substr(pointer, size));
+				pointer += size;
+			}
+		}
+		trace(acc);
+		return acc.map(to_token);
+	}
+
+	public function new() 
+	{
+		
 	}
 }
--- source/PlayState.hx
+++ source/PlayState.hx
@@ -1,23 +1,17 @@
 package;
 
 import flixel.FlxState;
-import sys.io.File;
 
 class PlayState extends FlxState
 {
 	override public function create():Void
 	{
-		//var chaine = 'FUNCTION HELLO(NAME) DO\n PRINT("HELLO WORLD")\nEND';
-		var chaine = File.getContent("assets/code/Test.txt");
-		var regex = ~/FUNCTION\s*([A-Z]+)\(([A-Z, ]*)\)\s*DO/;
-		//if (regex.match(chaine))
-		//{
-			//trace(regex.matched(1));
-			//trace(regex.matched(2));
-		//}
-		
-		var lexer = new Lexer(chaine);
-		lexer.lex();
+		var chaine = '(println "hello world")';
+		var tokens = Lexer.lex(chaine);
+		for (i in tokens) 
+		{
+			trace(i);
+		}
 		
 		super.create();
 	}
